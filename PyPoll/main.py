7ccade1d0db74cfafc9a506eebf7aa48e0e369b2
MrMrRPG <67204447+MrMrRPG@users.noreply.github.com>
2021-06-29 08:39:58 -0700

    Assignment Complete

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[173]:
+# In[105]:
 
 
 #imports
@@ -11,7 +11,7 @@ import pandas as pd
 import numpy as np
 
 
-# In[174]:
+# In[106]:
 
 
 # * In this challenge, you are tasked with helping a small, rural town modernize
@@ -43,7 +43,7 @@ import numpy as np
 # terminal and export a text file with the results.
 
 
-# In[175]:
+# In[107]:
 
 
 #set directory
@@ -52,8 +52,18 @@ csvPath = os.path.join(cwd, 'Resources', 'election_data.csv')
 df = pd.read_csv(csvPath)
 
 
-# In[225]:
+# In[ ]:
 
+
+
+
+
+# In[129]:
+
+
+#Setting some variables
+candidates = []
+candidatevote = []
 
 # opening and reading CSV module
 with open(csvPath) as csvfile:
@@ -65,15 +75,28 @@ with open(csvPath) as csvfile:
     pl_list = df['Voter ID'].tolist()
     county_list = df['County'].tolist()
     candidate_list = df['Candidate'].tolist()
-
-    # set data type
-    type(county_list)
-    type(candidate_list)
     
     #Total votes in data
     sum_votes = len(df)
-
-    # Adding all occurance of candidates
+    
+    #Looping through each row in raw data:
+    for row in csvreader:
+        name = row[2]
+        
+        if(name not in candidates):
+            candidates.append(name)
+            candidatevote.append(1)
+        else:
+            position = candidates.index(name)
+            candidatevote[position] += 1
+            
+    # Candidate Vote Percentage
+    vote_ratio_np = np.divide(candidatevote, sum_votes)
+    vote_ratio = vote_ratio_np.tolist()
+    
+    #finding the winner using index
+    winner_count_np = max(vote_ratio)
+    winner_index = vote_ratio.index(winner_count_np)
 
 def summary():
     # Print information
@@ -82,33 +105,52 @@ def summary():
     print(f"---------------------------------")
     print(f"Total Votes: {sum_votes}")
     print(f"---------------------------------")
-    print(f"")
-    print(f"")
-    print(f"")
-    print(f"")
+    
+    for candidate_index in range(len(candidates)):
+        if candidate_index == 0:
+            continue
+        else:
+            vote_ratio_fr = float(vote_ratio[candidate_index])
+            candidate_name = str(candidates[candidate_index])
+            votes = int(candidatevote[candidate_index])
+    
+        print(f"{candidate_name}: {vote_ratio_fr:.3%} ({votes})")
+    
     print(f"---------------------------------")
-    print(f"Winner: ")
+    print(f"Winner: {candidates[winner_index]}")
     print(f"---------------------------------")
 
 summary()
 
 
-# In[228]:
+# In[131]:
 
 
-#output to .txt file
-# def external(): 
-#     with open('Financial_Analysis.txt', 'w') as f:
-#         print(f"---------------------------------", file = f)
-#         print(f"FINANCIAL ANALYSIS", file = f)
-#         print(f"---------------------------------", file = f)
-#         print(f"Total Months: {sum_month}", file = f)
-#         print(f"Total Profit/Losses: ${sum_pl}", file = f)
-#         print(f"Average Change in Profit/Losses: ${round(avg_change_pl, 2)}", file = f)
-#         print(f"Greatest Increase in Profits: {maxincrease_day} (${maxincrease_pl})", file = f)
-#         print(f"Greatest Decrease in Profits: {minincrease_day} (${minincrease_pl})", file = f)
-#         print(f"---------------------------------", file = f)
-# external()
+# output to .txt file
+txtpath = os.path.join("analysis","analysis.txt")
+
+def external(): 
+    with open(txtpath, 'w') as f:
+        print(f"---------------------------------", file = f)
+        print(f"ELECTION RESULTS", file = f)
+        print(f"---------------------------------", file = f)
+        print(f"Total Votes: {sum_votes}", file = f)
+        print(f"---------------------------------", file = f)
+
+        for candidate_index in range(len(candidates)):
+            if candidate_index == 0:
+                continue
+            else:
+                vote_ratio_fr = float(vote_ratio[candidate_index])
+                candidate_name = str(candidates[candidate_index])
+                votes = int(candidatevote[candidate_index])
+
+            print(f"{candidate_name}: {vote_ratio_fr:.3%} ({votes})", file = f)
+
+        print(f"---------------------------------", file = f)
+        print(f"Winner: {candidates[winner_index]}", file = f)
+        print(f"---------------------------------", file = f)
+external()
 
 
 # In[ ]:
